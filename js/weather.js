15aeb9e5b7ac87a8e46cdd62b36f28e679fc83cd
jgj8920@gmail.com <jgj8920@gmail.com>
2023-10-09 23:17:08 +0900

    commit

--- js/weather.js
+++ js/weather.js
@@ -5,7 +5,6 @@ const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
